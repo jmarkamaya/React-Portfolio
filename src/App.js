5b9b0d5d0f1bccff0fc9801db9615c3bfbe64b7d
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-09-19 18:36:35 -0700

    created react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Page from "./components/Page";
+import Footer from "./components/Footer";
+
+function App() {
+ 
+
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
