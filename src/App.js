d6cdeccd70872c15bd900c8b1971b94ba475422b
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-09-19 19:43:03 -0700

    added footer and font awesome

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 // import Header from "./components/Header";
 // import Nav from "./components/Nav";
 import Page from "./components/Page";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
  
 
   return (
     <div>
-     <Page />
+      <main>
+      <Page />
+      </main>
+
+      <Footer />
+    
     </div>
   );
 }
